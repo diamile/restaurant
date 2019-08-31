06277b2779049bae9bc5497f97e597af2bced078
sarr <diamilesarr2006@gmail.com>
2019-08-31 15:50:11 +0200

    relations entre les tables

--- app/Day.php
+++ app/Day.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Day extends Model
 {
-    //
+    public function restaurants()
+	{
+		return $this->belongsToMany('App\Restaurant', 'horaires')->withPivot('id', 'start_time', 'end_time');
+	}
 }
--- app/Restaurant.php
+++ app/Restaurant.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+    public function days()
+	{
+		return $this->belongsToMany('App\Day', 'horaires')->withPivot('start_time', 'end_time');
+	}
+
 }
